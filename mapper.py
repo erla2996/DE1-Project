3022fd0e632bf87d4f6e706aa3dfdc020e4dd3a5
Team 1 <you@example.com>
2021-03-24 18:29:02 +0000

    cleaned up comments

--- mapper.py
+++ mapper.py
@@ -5,6 +5,7 @@
 
 import tables
 
+# Code from hdf5_getters.py
 
 def open_h5_file_read(h5filename):
     """
@@ -450,11 +451,7 @@ def get_year(h5,songidx=0):
     return h5.root.musicbrainz.songs.cols.year[songidx]
 
 import sys
-#sys.path.append('.')
 import math
-
-# imports specific to the MSD
-#import hdf5_getters as GETTERS
 
 def read_input(file):
     i=0 #Counts the number of rows in the file
@@ -467,10 +464,6 @@ def read_input(file):
     #Variables for calculating conditional probabilities for a given feature:
     k=0  #Count low hotness
     kk=0 #Count high hotness
-    #prob_low_feat_low_hot = 0  #Initialize probabilities as zero
-    #prob_high_feat_low_hot = 0  #Initialize probabilities as zero
-    #prob_low_feat_high_hot = 0  #Initialize probabilities as zero
-    #prob_high_feat_high_hot = 0  #Initialize probabilities as zero
     low_feat_low_hot = 0  #Initialize counts as zero 
     high_feat_low_hot = 0  #Initialize counts as zero 
     low_feat_high_hot = 0  #Initialize counts as zero 
@@ -479,9 +472,6 @@ def read_input(file):
         line = str(line.strip("\n"))    #line.strip()
         line = line[1:]
         line = "/home/ubuntu/" + line
-        #raise Exception(line)
-        #print line
-        #line = "hdfs://node-master:9000" + line
         if line != '\n' and line != '':
             i += 1
             # Open input as an h5 file
@@ -542,49 +532,6 @@ def read_input(file):
             except AttributeError:
                 duration = None
                 Duration = 0 #Assign zero if Null
-            #try:
-            #    time_signature = GETTERS.get_time_signature(h5)
-            #except AttributeError:
-            #    time_signature = None
-            #try:
-            #    end_of_fade_in = GETTERS.get_end_of_fade_in(h5)
-            #except AttributeError:
-            #    end_of_fade_in = None
-           # try:
-           #     danceability = danceability(h5)
-           #     if (not math.isnan(danceability)):
-           #         Danceability = danceability
-           #     else:
-           #         Danceability = 0
-           # except AttributeError:
-           #     danceability = None
-           #     Danceability = 0
-            
-            ##Calculate conditional probability:
-            #feat_prob = Duration
-            #if (feat_prob != None and not math.isnan(feat_prob) and feat_prob != 0):  #Only calculate for legitimate values
-            #    if (hot_song == 1):
-            #        k += 1  #Count how many you sum up for low hotness
-            #        if (feat_prob == 1):
-            #            low_feat_low_hot += 1
-            #        elif (feat_prob == 2):
-            #            high_feat_low_hot += 1
-            #    elif (hot_song == 2):
-            #        kk += 1  #Count how many you sum up for high hotness
-            #        if (feat_prob == 1):
-            #            low_feat_high_hot += 1
-            #        elif (feat_prob == 2):
-            #            high_feat_high_hot += 1                 
-            #        prob_low_feat_low_hot = low_feat_low_hot / k  #Conditional probability of getting low feature value given low song hotness
-            #        prob_high_feat_low_hot = high_feat_low_hot / k   #Conditional probability of getting high feature value given low song hotness
-            #        prob_low_feat_high_hot = low_feat_high_hot / kk  #Conditional probability of getting low feature value given high song hotness
-            #        prob_high_feat_high_hot = high_feat_high_hot / kk  #Conditional probability of getting high feature value given high song hotness
-
-            #feat = danceability  #Input feature that you want to calculate the mean value for!
-            #if (feat != None and not math.isnan(feat) and feat != 0):  #Only calculate for legitimate values
-            #    feature += feat #Sum up the features
-            #    j += 1.0  #Calculate count how many you sum up
-            #    mean_of_feature = feature / j #Divide sum by number of items to get the mean
             # Return the gotten attributes
             if (song_hotttnesss != None and not (math.isnan(song_hotttnesss))):   #Uncomment if you only want the values where song_hotttnesss is defined
                 yield (hot_song, Tempo, Year, Duration)  #, danceability)  # mean_of_feature) #, duration, time_signature, end_of_fade_in)
